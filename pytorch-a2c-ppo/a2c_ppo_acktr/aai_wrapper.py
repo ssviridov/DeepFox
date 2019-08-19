c6b7cde71f29e790cc0cf9d044c9e129ed82345d
ssviridov <sergei.sviridov@gmail.com>
2019-08-19 22:31:29 +0300

    Added proper FrameStackWrapper import

--- pytorch-a2c-ppo/a2c_ppo_acktr/aai_wrapper.py
+++ pytorch-a2c-ppo/a2c_ppo_acktr/aai_wrapper.py
@@ -14,7 +14,7 @@ from baselines import bench
 from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
 from baselines.common.vec_env.dummy_vec_env import DummyVecEnv
 
-from a2c_ppo_acktr.envs import TransposeImage, VecPyTorch, VecPyTorchFrameStack, ShmemVecEnv
+from a2c_ppo_acktr.envs import TransposeImage, VecPyTorch, VecPyTorchFrameStack, VecPyTorchFrameStackDictObs, ShmemVecEnv
 from .aai_config_generator import SingleConfigGenerator
 
 from .aai_env_fixed import UnityEnvHeadless
